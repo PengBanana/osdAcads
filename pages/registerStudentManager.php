8b9b6a5c230245259e7a54c645dbc4ab78303b12
PengBanana <thealvinsep@gmail.com>
2017-11-30 05:45:22 +0800

    accountRegistration
    error checking added

--- pages/registerStudentManager.php
+++ pages/registerStudentManager.php
@@ -2,6 +2,7 @@
 session_start();
 //INSERT INTO `acadsosd`.`team` (`sportCode`, `teamName`, `teamSport`) VALUES ('BBM', 'Green Archers', 'Basketball Men');
 if(isset($_POST['registerManager'])){
+	//get all the data from the form and prepare for sql insert
 	require_once('../osd_connect.php');
 	$idNumber=$_POST['idNumber'];
 	$firstName=$_POST['firstName'];
@@ -9,28 +10,54 @@ if(isset($_POST['registerManager'])){
 	$lastName=$_POST['lastName'];
 	$email=$_POST['email'];
 	$password=substr(hash('sha512',rand()),0,6);;
-	$query="INSERT INTO `acadsosd`.`user` (`idnumber`, `usertypeID`, `firstName`, `lastName`, `middleName`, `password`) VALUES ('".$idNumber."', '3', '".$firstName."', '".$lastName."', '".$middleName."', '".$password."');";
-	mysqli_query($dbc,$query);
-	$query="INSERT INTO `acadsosd`.`studentmanager` (`idnumber`, `email`, `managerCode`) VALUES ('".$idNumber."', '".$email."', '3');";
-	mysqli_query($dbc,$query);
-	header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/studentManagerForApproval.php");
+	//checking if user exist already
+	$query='SELECT * FROM acadsosd.studentmanager WHERE idnumber="'.$idNumber.'";';
+	$result=mysqli_query($dbc, $query);
+	$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+	$checker=$row['idnumber'];
+	if(empty($checker)){
+		//insert the new user sql
+		$query="INSERT INTO `acadsosd`.`user` (`idnumber`, `usertypeID`, `firstName`, `lastName`, `middleName`, `password`) VALUES ('".$idNumber."', '3', '".$firstName."', '".$lastName."', '".$middleName."', '".$password."');";
+		mysqli_query($dbc,$query);
+		$query="INSERT INTO `acadsosd`.`studentmanager` (`idnumber`, `email`, `managerCode`) VALUES ('".$idNumber."', '".$email."', '3');";
+		mysqli_query($dbc,$query);
+		header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/studentManagerForApproval.php");	
+	}
+	else{
+		$checker=$row['managerCode'];
+			if($checker==2){
+				//proceeds with registration if the account was just deactivated
+				$query="UPDATE `acadsosd`.`studentmanager` SET `managerCode`='3' WHERE `idnumber`='".$idNumber."';";
+				mysqli_query($dbc,$query);
+				header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/reactivation.php");	
+			}
+			else if($checker==3){
+				//if account is currently for activation
+				echo '<div class="alert alert-danger">
+					Please wait for your administrator to activate your account.
+					</div>';
+			}else if($checker==1){
+				//if account is currently for activation
+				echo '<div class="alert alert-danger">
+					Your account is already active.
+					</div>';
+			}
+			else if($checker==4){
+				//if account was rejected but wants to retry registration
+				$query="UPDATE `acadsosd`.`studentmanager` SET `managerCode`='3' WHERE `idnumber`='".$idNumber."';";
+				mysqli_query($dbc,$query);
+				header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/reactivation.php");	
+			}
+			else{
+				echo '<div class="alert alert-danger">
+					There is a problem with your account. Please contact your administrator.
+					</div>';
+			}
+	}
+	
+	
 }
 ?>
-<!--
-<html>
-<head>
-</head>
-<body>
-<form action="registerStudentManager.php" method="post">
-ID Number: <input type="text" name="idNumber" maxlength="45" />
-First Name: <input type="text" name="firstName" maxlength="45" />
-Middle Name: <input type="text" name="middleName" maxlength="45" />
-Last Name: <input type="text" name="lastName" maxlength="45" />
-Email: <input type="text" name="email" maxlength="45" />
-</form>
-</body>
-</html>
--->
 <!DOCTYPE html>
 <html lang="en">
 
