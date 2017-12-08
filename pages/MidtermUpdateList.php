3999b15ccda5a011e44ede8963c881ce738a6a31
PengBanana <thealvinsep@gmail.com>
2017-12-09 03:43:22 +0800

    midTermUpdateList.php

--- pages/MidtermUpdateList.php
+++ pages/MidtermUpdateList.php
@@ -1,31 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<?php 
-  session_start();
-  require_once('../osd_connect.php');
-  $fname = $_SESSION["name"];
-  $idNum = $_SESSION["idnumber"];
-  $userType = $_SESSION["typex"];
-
-  if(empty($idNum)){
-    header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/login.php");
-  }
-  else if(empty($fname)){
-    header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/login.php");
-  }
-  else if(empty($userType)){
-    header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/login.php");
-  }
-
-
-  $teamCodeQuery = "SELECT teamCode FROM studentManager WHERE idnumber = '".$idNum."'";
-  $teamCodeResult = mysqli_query($dbc, $teamCodeQuery);
-  $teamCodeRow = mysqli_fetch_array($teamCodeResult, MYSQLI_ASSOC);
-  $teamCodeFinal = $teamCodeRow['teamCode'];
-
-
+<?php
+session_start();
+require_once('../osd_connect.php');
+$idx=$_SESSION['idnumber'];
+					$typex=$_SESSION["typex"];
+					if($idx===0){
+					header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/login.php");
+					}
+					if(empty($idx)){
+						header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/login.php");
+						$name="wth";
+					}
+					else if($typex>2||$typex<1){
+						header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/invalidRequest.php");
+					}
+					else if(empty($typex)){
+						header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/login.php");
+					}
+					else{
+						$name=$_SESSION["name"];
+					}
 ?>
+<html lang="en">
 
 <head>
 
@@ -68,167 +64,137 @@
 </head>
 
 <body>
-  <!-- Navigation -->
- <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0" id="up">
-      <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand logo" style="padding: 10px 0px 0px 30px" href="index[studentManager].php">
-          <img src="Images/OSD-logo2.png" height="35px" width='auto' />
-          </a>
-      </div>
 
-      <!-- /.navbar-header -->
+    <div id="wrapper">
+			<!-- Navigation -->
+		 <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0" id="up">
+					<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand logo" style="padding: 10px 0px 0px 30px" href="index[admin].php">
+							<img src="Images/OSD-logo2.png" height="35px" width='auto' />
+							</a>
+					</div>
 
-      <!-- NAV BAR -->
+					<!-- /.navbar-header -->
 
-      <ul class="nav navbar-top-links navbar-right">
+					<!-- NAV BAR -->
 
-          <!-- /.dropdown -->
-          <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                  <i class="fa fa-user fa-fw" style="color: white"></i>  <i class="fa fa-caret-down" style="color: white"></i>
-              </a>
-              <ul class="dropdown-menu dropdown-user" >
-                  <li><a href="changePassword.php"><i class="fa fa-gear fa-fw"></i> Change Password</a>
-                  </li>
-                  <li class="divider"></li>
-                  <li><a href="login.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                  </li>
-              </ul>
-              <!-- /.dropdown-user -->
-          </li>
-          <!-- /.dropdown -->
-      </ul>
-      <!-- /.navbar-top-links -->
+					<ul class="nav navbar-top-links navbar-right">
 
-      <div class="navbar-fixed sidebar"  role="navigation" >
-          <div class="sidebar-nav navbar-f" >
-              <ul class="nav" id="side-menu" >
+							<!-- /.dropdown -->
+							<li class="dropdown">
+									<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+											<i class="fa fa-user fa-fw" style="color: white"></i>  <i class="fa fa-caret-down" style="color: white"></i>
+									</a>
+									<ul class="dropdown-menu dropdown-user" >
+											<li><a href="changePassword.php"><i class="fa fa-gear fa-fw"></i> Change Password</a>
+											</li>
+											<li class="divider"></li>
+											<li><a href="login.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+											</li>
+									</ul>
+									<!-- /.dropdown-user -->
+							</li>
+							<!-- /.dropdown -->
+					</ul>
+					<!-- /.navbar-top-links -->
 
-                      <li >
-                          <a href="index[studentManager].php"><i class="glyphicon glyphicon-home" style="color: white"></i> Home</a>
-                      </li>
+					<div class="navbar-fixed sidebar"  role="navigation" >
+							<div class="sidebar-nav navbar-f" >
+									<ul class="nav" id="side-menu" >
 
-                       <li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="    glyphicon glyphicon-folder-open" style="color: white"></i>    Academic Performance</i></a>
-                        <ul id="demo1" class="collapse" style="list-style: none;">
-                          <li><a href="MidtermUpdateList.php"><i class="glyphicon glyphicon-menu-right"  style="color: white" style ></i> Midterm Updates </a></li>
-                          <li><a href="FinalsUpdateList.php"><i class="glyphicon glyphicon-menu-right"  style="color: white" style ></i> Final Updates </a></li>
-                        </ul>
-                        </li>
-                        <li>
-                            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="glyphicon glyphicon-certificate" style="color: white"></i>    Team </a>
-                            <ul id="demo4" class="collapse" style="list-style: none;">
-                               <li><a href="ViewTeamAthletes.php" style="font-size: 11px;"><i class="glyphicon glyphicon-menu-right"  style="color: white"></i> View Varsity Teams </a></li>
+													<li >
+															<a href="index[studentManager].php"><i class="glyphicon glyphicon-home" style="color: white"></i> Home</a>
+													</li>
 
-                            </ul>
-                        </li>
+													 <li>
+														<a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="    glyphicon glyphicon-folder-open" style="color: white"></i>    Academic Performance</i></a>
+														<ul id="demo1" class="collapse" style="list-style: none;">
+															<li><a href="MidtermUpdateList.php"><i class="glyphicon glyphicon-menu-right"  style="color: white" style ></i> Midterm Updates </a></li>
+															<li><a href="FinalsUpdateList.php"><i class="glyphicon glyphicon-menu-right"  style="color: white" style ></i> Final Updates </a></li>
+														</ul>
+														</li>
+														<li>
+																<a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="glyphicon glyphicon-certificate" style="color: white"></i>    Team </a>
+																<ul id="demo4" class="collapse" style="list-style: none;">
+																	 <li><a href="ViewTeamAthletes_admin.php" style="font-size: 11px;"><i class="glyphicon glyphicon-menu-right"  style="color: white"></i> View Varsity Teams </a></li>
 
-              </ul>
-          </div>
-          <!-- /.sidebar-collapse -->
-      </div>
-      <!-- /.navbar-static-side -->
-  </nav>
+																</ul>
+														</li>
 
-    <div id="wrapper" >
-
+									</ul>
+							</div>
+							<!-- /.sidebar-collapse -->
+					</div>
+					<!-- /.navbar-static-side -->
+			</nav>
 
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <?php 
+                    <h1 class="page-header">Select a team</h1>
 
-                      $teamNameQuery = "SELECT teamName FROM team WHERE sportCode = '".$teamCodeFinal."'";
-                      $teamNameResult = mysqli_query($dbc, $teamNameQuery);
-                      $teamNameRow = mysqli_fetch_array($teamNameResult,MYSQLI_ASSOC);
-                      $teamNameFinal = $teamNameRow['teamName'];
+                <div>
 
-                    ?>
-                    <h1 class="page-header">Midterm Updates</h1>
-                </div>
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
-
-
-
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            <label><b><?php echo $teamNameFinal ?></b><label>
+                           View Midterm Updates
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
-                            <div class="dataTable_wrapper">
+                            <div class="dataTable_wrapper" >
                                 <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                                     <thead>
                                         <tr>
-                                            <th class="text-center">Student Athlete</th>
-                                            <th class="text-center">Course</th>
-                                            <th class="text-center">Professor</th>
-                                            <th class="text-center">Midterm Grade</th>
-                                            <th class="text-center">Midterm Update</th>
-
+                                            <th class="text-center">TEAM CODE</th>
+                                            <th class="text-center">TEAM NAME</th>
+                                            <th class="text-center">TEAM SPORT</th>
+                                            <th class="text-center">No. Athetes</th>
+                                            <th class="text-center">Action</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        
-                                          <?php
 
-                                            $allQuery = "SELECT CONCAT(sap.studentLastName, ', ', sap.studentFirstName) as saFullName, sd.courseCode as courseCode, sd.midtermAcademicReport as MAR, sd.midtermGrade as midtermGrade, p.professorName as profName
-                                                         FROM studentathleteprofile sap
-                                                         JOIN plannedenrollmentchart pec ON sap.studentidnumber = pec.studentidnumber
-                                                         JOIN subjectdetails sd ON pec.pecID = sd.PlannedEnrollmentChart_pecID
-                                                         JOIN professor p ON sd.professorID = p.professorID WHERE sap.teamCode = '".$teamCodeFinal."';";
-                                            $saNameQuery = "SELECT CONCAT(studentLastName, ', ', studentFirstName) as saFullName FROM studentathleteprofile";
-                                            $allQueryResult = mysqli_query($dbc, $allQuery);
+                                    <?php
+                                    $getDetails = "SELECT T.SPORTCODE, T.TEAMNAME, T.SPORT, IFNULL(COUNT(SA.STUDENTIDNUMBER), 0) AS NUMBEROFSTUDENTS
+                                                   FROM TEAM T LEFT JOIN STUDENTATHLETEPROFILE SA ON T.SPORTCODE = SA.TEAMCODE
+                                                   GROUP BY T.SPORTCODE;";
+                                    $result=mysqli_query($dbc,$getDetails);
 
-                                          ?>
+                                    while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+                                        $sportcode=$row['SPORTCODE'];
+                                        echo'
+                                        <tr class="odd gradeX">
+                                             <td>
+												<div align="center"><label>'.$sportcode.'</label></div></td>
+                                             <td><div align="center">'.$row['TEAMNAME'].'</div></td>
+                                             <td><div align="center">'.$row['SPORT'].'</div></td>
+                                             <td><div align="center">'.$row['NUMBEROFSTUDENTS'].'</div></td>
+                                             <td><div align="center"><form action="updateList.php" method="post">
+											 <input type="hidden" name="sportCode" value="'.$sportcode.'">
+												<div align="center"><input type="submit" value="View Updates" class="btn btn-link" name="viewMU"></div>
+												</form></div></td>
+                                        </tr>';
 
-                                          <?php
-                                            foreach ($allQueryResult as $row) {
-                                            ?>
-                                            <tr class="odd gradeA">
-                                            <td class="text-center">
-                                              <?php echo $row['saFullName'];?>
-                                            </td>
-                                            <td class="text-center">
-                                              <?php echo $row['courseCode'];?>
-                                            </td>
-                                            <td class="text-center">
-                                              <?php echo $row['profName'];?>
-                                            </td>
-                                            <td class="text-center">
-                                              <?php echo $row['midtermGrade'];?>
-                                            </td>
-                                            <td class="text-center"><p>
-                                              <?php echo $row['MAR'];?>
-                                            </p></td>
-                                          </tr>
-                                            
-                                        <?php }?>
-											                      
+                                    }
 
-                                    </tbody>
+                                    ?>
                                 </table>
 
 
-                                </div>
-
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
 
 
-                </div>
+
                             </div>
 
                         </div>
